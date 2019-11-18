ec0472892c317de265e8a6c4d5c5821992f56bdf
KasraTabrizi <kasra_tabrizi@hotmail.com>
2019-11-18 14:35:04 +0100

    added eventlistener on search button

--- script/script.js
+++ script/script.js
@@ -6,48 +6,55 @@ let cityName = "Gent";
 let cityObj;
 const arrayData = [];
 
-//parse json file with all the citynames and id
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        cityObj = JSON.parse(this.responseText);
-        for (element of cityObj) {
-            if (element.name == cityName) {
-                cityId = String(element.id);
-            }
-        }
-        apiLink = `http://api.openweathermap.org/data/2.5/forecast?id=${cityId}&units=metric&APPID=${apiKey}`;
-        console.log(apiLink);
 
-        //Fetch data from openweathermap API
-        fetch(apiLink)
-            .then(res => res.json())
-            .then(weatherObj => {
-                console.log(weatherObj);
-                arrayData.push(weatherObj.city.name);
-                arrayData.push(weatherObj.city.country);
-                for (data of weatherObj.list) {
-                    console.log(data);
-                    let dateTime = data.dt_txt.split(" ");
-                    if (dateTime[1] === "18:00:00") {
-                        //push date and time
-                        arrayData.push(dateTime[0]);
-                        arrayData.push(dateTime[1]);
-                        //push main temp
-                        arrayData.push(data.main.temp);
-                        //push cloudiness description
-                        arrayData.push(data.weather[0].description);
-                        //push humidity
-                        arrayData.push(data.main.humidity);
-                        //push pressure
-                        arrayData.push(data.main.pressure);
-                        //push wind speed
-                        arrayData.push(data.wind.speed);
-                    }
+document.getElementById("").addEventListener("click", () => {
+
+    //get city name from input text box
+    let inputText = document.getElementById("").value;
+
+    //parse json file with all the citynames and id
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cityObj = JSON.parse(this.responseText);
+            for (element of cityObj) {
+                if (element.name == inputText) {
+                    cityId = String(element.id);
                 }
-            });
-        console.log(arrayData);
-    }
-};
-xmlhttp.open("GET", "script/citylist.json", true);
-xmlhttp.send();
+            }
+            apiLink = `http://api.openweathermap.org/data/2.5/forecast?id=${cityId}&units=metric&APPID=${apiKey}`;
+            console.log(apiLink);
+
+            //Fetch data from openweathermap API
+            fetch(apiLink)
+                .then(res => res.json())
+                .then(weatherObj => {
+                    console.log(weatherObj);
+                    arrayData.push(weatherObj.city.name);
+                    arrayData.push(weatherObj.city.country);
+                    for (data of weatherObj.list) {
+                        console.log(data);
+                        let dateTime = data.dt_txt.split(" ");
+                        if (dateTime[1] === "18:00:00") {
+                            //push date and time
+                            arrayData.push(dateTime[0]);
+                            arrayData.push(dateTime[1]);
+                            //push main temp
+                            arrayData.push(data.main.temp);
+                            //push cloudiness description
+                            arrayData.push(data.weather[0].description);
+                            //push humidity
+                            arrayData.push(data.main.humidity);
+                            //push pressure
+                            arrayData.push(data.main.pressure);
+                            //push wind speed
+                            arrayData.push(data.wind.speed);
+                        }
+                    }
+                });
+            console.log(arrayData);
+        }
+    };
+    xmlhttp.open("GET", "script/citylist.json", true);
+    xmlhttp.send();
+});
