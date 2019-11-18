d15649fbfddf0f875b103bb177853bab462d00df
KasraTabrizi <kasra_tabrizi@hotmail.com>
2019-11-18 09:42:59 +0100

    added citylink as example

--- script/script.js
+++ script/script.js
@@ -1,3 +1,3 @@
-let apiKey = "076fb559d233df5c4fb8adbeba1901ab";
-let cityId = 0;
+let apiKey = "ef560ab659e3a9522467daea187b0400";
+let cityId = "2797657";
 let apiLink = `http://api.openweathermap.org/data/2.5/forecast?id=${cityId}&APPID=${apiKey}`;
