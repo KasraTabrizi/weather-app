1264fb8029a33b00d20c6aadc71d5948e749c643
KasraTabrizi <kasra_tabrizi@hotmail.com>
2019-11-18 10:05:52 +0100

    added metric to the link to give data in celsius

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 let apiKey = "ef560ab659e3a9522467daea187b0400";
 let cityId = "2797657";
-let apiLink = `http://api.openweathermap.org/data/2.5/forecast?id=${cityId}&APPID=${apiKey}`;
+let apiLink = `http://api.openweathermap.org/data/2.5/forecast?id=${cityId}&units=metric&APPID=${apiKey}`;
 
 fetch(apiLink)
     .then(res => res.json())
