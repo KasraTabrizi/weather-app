e3b2cd4ccd57dd8264cc0585a15a8a6e35f458f1
KasraTabrizi <kasra_tabrizi@hotmail.com>
2019-12-03 11:35:58 +0100

    all de data is printed in the correct way

--- index.php
+++ index.php
@@ -23,196 +23,105 @@
             </div>
         </div>
     <?php
-
         define("apiKey","ef560ab659e3a9522467daea187b0400"); //declare constant variable for the API key
         define("days",["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"] ); //declare constant variable for the days of the week
-        $cityId = "2797657"; //ID Gent
-        //$apiLink = 'http://api.openweathermap.org/data/2.5/forecast?id=' . urlencode($cityId) . '&units=metric&APPID=' . urlencode(apiKey);
+        $cityId = ''; 
         $apiLink = 'http://api.openweathermap.org/data/2.5/forecast?id=' . $cityId . '&units=metric&APPID=' . apiKey;
-        $cityObj;
 
         if (isset($_POST["cityname"])) { //get the cityname from the form
             $cityName = $_POST["cityname"];
             //fetch data from citylist.json to find the id of the city
             $jsonCityList = file_get_contents('script/citylist.json');
             
-            $arrayCityList = json_decode($jsonCityList, true);
+            //fetch jsoncitylist and convert it into an array
+            $arrayCityList = json_decode($jsonCityList, true); 
             //echo var_dump($arrayCityList);
             //loop through all the citynames untill you find a match and then return id
             foreach( $arrayCityList as $value){
                 if ($value['name'] == $cityName){
-                    //echo $value['name'];
-                    //echo $value['id'];
                     $cityId = $value['id'];
                 break;
                 }
             }
-
             $apiLink = 'http://api.openweathermap.org/data/2.5/forecast?id=' . $cityId . '&units=metric&APPID=' . apiKey;
             //fetch data from the weather app
             $json_data = file_get_contents($apiLink);
 
             $user_data = json_decode($json_data, true); //decode json into an array
-            //echo var_dump($user_data['list']['0']['weather']['0']['description']);
-            //echo var_dump($user_data);
             $user_data_length = count($user_data['list']); //get the lenght of the list
-            //echo $user_data_length;
+
+            //HTML TABLE SECTION
+            echo '<h2 id="cityname">';
+            echo $user_data['city']['name'] . ', ' . $user_data['city']['country'];
+            echo '</h2>';
+
+            echo '<div id="wrapperbox">';
+            //for loop
             for ($x = 0; $x < $user_data_length; $x++) {
-                //echo var_dump($user_data['list'][strval($x)]['main']['temp']);
+                //split dt_txt into date and time = $split[0] = date & $split[1] = time
+                $datum = $user_data['list'][strval($x)]['dt_txt'];
+                $split = explode(" ", $datum);
+                $dayName = date('l', strtotime($split[0]));
+                if ( $split[1] == "15:00:00" ){ //only print when the time is equal to the desired time.
+                    echo '<div class="box">';
+                    echo '<h3 class="datum">';
+                    echo $split[0];
+                    echo '</h3>';
+                    echo '<h3 class="nameday">';
+                    echo $dayName;
+                    echo '</h3>';
+
+                    echo '<div class="day">';
+                    echo'<p class="hour">';
+                    echo $split[1]; 
+                    echo'</p>'; //end class hour
+                    $iconName = $user_data['list'][strval($x)]['weather']['0']['icon'];
+                    $iconLink = 'http://www.openweathermap.org/img/w/' . $iconName . '.png';
+                    echo '<div class="image">';
+                    echo '<img src=' . $iconLink . ' alt="icon">';
+                    echo '</div>'; //end class image
+                    echo '</div>'; //end class day
+                    echo '<p class="maintemp">';
+                    echo $user_data['list'][strval($x)]['main']['temp'];
+                    echo '°C</p>'; // end class maintemp
+                    
+                    echo '<table class="customers">';
+
+                    echo '<tr>';
+                    echo '<td>Cloudiness</td>';
+                    echo '<td class="cloud">';
+                    echo $user_data['list'][strval($x)]['weather']['0']['description'];
+                    echo '</td>';
+                    echo '</tr>'; //end tr 1
+                    
+                    echo '<tr>';
+                    echo '<td>Humidity</td>';
+                    echo '<td class="humidity">';
+                    echo $user_data['list'][strval($x)]['main']['humidity'];
+                    echo '%</td>';
+                    echo '</tr>'; //end tr 2
+
+                    echo '<tr>';
+                    echo '<td>Pressure</td>';
+                    echo '<td class="pressure">';
+                    echo $user_data['list'][strval($x)]['main']['pressure'];
+                    echo 'hpa</td>';
+                    echo '</tr>'; //end tr 3
+
+                    echo '<tr>';
+                    echo '<td>Wind</td>';
+                    echo '<td class="wind">';
+                    echo $user_data['list'][strval($x)]['wind']['speed'];
+                    echo 'm/s</td>';
+                    echo '</tr>'; //end tr 4
+
+                    echo '</table>'; //end table element
+                    echo '</div>'; //end div class box
+                } 
             }
-            //echo var_dump($user_data['city']['id']);
-           
+            echo '</div>'; //end wrapperbox
         }
-
         ?>
-
-        <h2 id="cityname"><?php echo $user_data['city']['name'] . ', ' . $user_data['city']['country'];?></h2>
-        <div id="wrapperbox">
-            <div class="box">
-                <?php 
-                    $datum = $user_data['list']['0']['dt_txt'];
-                    $split = explode(" ", $datum);
-                    $dayName = date('l', strtotime($split[0]));
-                ?>
-                <h3 class="datum"><?php echo $split[0]; ?></h3>
-                <h3 class="nameday"><?php echo $dayName; ?></h3>
-                <div class="day">
-                    <p class="hour"><?php echo $split[1]; ?></p>
-                    <?php 
-                        $iconName = $user_data['list']['0']['weather']['0']['icon'];
-                        $iconLink = "http://www.openweathermap.org/img/w/".$iconName.".png";
-                    ?>
-                    <div class="image"></div><img src=<?php echo $iconLink;?> alt="icon">
-                </div>
-                <p class="maintemp"><?php echo $user_data['list']['0']['main']['temp'];?>°C</p>
-                <table class="customers">
-                    <tr>
-                        <td>Cloudiness</td>
-                        <td class="cloud"><?php echo $user_data['list']['0']['weather']['0']['description'];?></td>
-                    </tr>
-                    <tr>
-                        <td>Humidity</td>
-                        <td class="humidity"><?php echo $user_data['list']['0']['main']['humidity'];?>%</td>
-                    </tr>
-                    <tr>
-                        <td>Pressure</td>
-                        <td class="pressure"><?php echo $user_data['list']['0']['main']['pressure'];?>hpa</td>
-                    </tr>
-                    <tr>
-                        <td>Wind</td>
-                        <td class="wind"><?php echo $user_data['list']['0']['wind']['speed'];?>m/s</td>
-                    </tr>
-                </table>
-            </div>
-            <div class="box">
-                <h3 class="datum">Datum</h3>
-                <h3 class="nameday">Day</h3>
-                <div class="day">
-                    <p class="hour">hour</p>
-                    <div class="image"><img src="" alt="icon"></div>
-                    <p class="maintemp"></p>
-                    <table class="customers">
-                        <tr>
-                            <td>Cloudiness</td>
-                            <td class="cloud">/</td>
-                        </tr>
-                        <tr>
-                            <td>Humidity</td>
-                            <td class="humidity">/</td>
-                        </tr>
-                        <tr>
-                            <td>Pressure</td>
-                            <td class="pressure">/</td>
-                        </tr>
-                        <tr>
-                            <td>Wind</td>
-                            <td class="wind">/</td>
-                        </tr>
-                    </table>
-                </div>
-            </div>
-            <div class="box">
-                <h3 class="datum">Datum</h3>
-                <h3 class="nameday">Day</h3>
-                <div class="day">
-                    <p class="hour">hour</p>
-                    <div class="image"><img src="" alt="icon"></div>
-                    <p class="maintemp"></p>
-                    <table class="customers">
-                        <tr>
-                            <td>Cloudiness</td>
-                            <td class="cloud">/</td>
-                        </tr>
-                        <tr>
-                            <td>Humidity</td>
-                            <td class="humidity">/</td>
-                        </tr>
-                        <tr>
-                            <td>Pressure</td>
-                            <td class="pressure">/</td>
-                        </tr>
-                        <tr>
-                            <td>Wind</td>
-                            <td class="wind">/</td>
-                        </tr>
-                    </table>
-                </div>
-            </div>
-            <div class="box">
-                <h3 class="datum">Datum</h3>
-                <h3 class="nameday">Day</h3>
-                <div class="day">
-                    <p class="hour">hour</p>
-                    <div class="image"><img src="" alt="icon"></div>
-                    <p class="maintemp"></p>
-                    <table class="customers">
-                        <tr>
-                            <td>Cloudiness</td>
-                            <td class="cloud">/</td>
-                        </tr>
-                        <tr>
-                            <td>Humidity</td>
-                            <td class="humidity">/</td>
-                        </tr>
-                        <tr>
-                            <td>Pressure</td>
-                            <td class="pressure">/</td>
-                        </tr>
-                        <tr>
-                            <td>Wind</td>
-                            <td class="wind">/</td>
-                        </tr>
-                    </table>
-                </div>
-            </div>
-            <div class="box">
-                <h3 class="datum">Datum</h3>
-                <h3 class="nameday">Day</h3>
-                <div class="day">
-                    <p class="hour">hour</p>
-                    <div class="image"><img src="" alt="icon"></div>
-                    <p class="maintemp"></p>
-                    <table class="customers">
-                        <tr>
-                            <td>Cloudiness</td>
-                            <td class="cloud">/</td>
-                        </tr>
-                        <tr>
-                            <td>Humidity</td>
-                            <td class="humidity">/</td>
-                        </tr>
-                        <tr>
-                            <td>Pressure</td>
-                            <td class="pressure">/</td>
-                        </tr>
-                        <tr>
-                            <td>Wind</td>
-                            <td class="wind">/</td>
-                        </tr>
-                    </table>
-                </div>
-            </div>
-        </div>
     </div>
 </body>
 
